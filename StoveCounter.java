369d2869476984952a9e7ca951463d999aede605
person-030 <abigailyang783@gmail.com>
2025-12-12 19:07:09 -0500

    Kelly

--- StoveCounter.java
+++ StoveCounter.java
@@ -8,42 +8,5 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class StoveCounter extends Counter
 {
-    private GreenfootImage stoveCounter = new GreenfootImage ("images/stoveCounter.PNG");
-    private GreenfootImage selectedStoveCounter = new GreenfootImage ("images/selectedStoveCounter.PNG");
-    private GreenfootImage startingPot = new GreenfootImage("images/emptyPot.PNG");
-    private GreenfootImage currentPot = new GreenfootImage("images/emptyPot.PNG");
-    private Pots pots = new Pots();
-    int offset = 5;
-    int width = 60;
     
-    public StoveCounter() {
-        stoveCounter.scale(width, width);
-        selectedStoveCounter.scale(width, width);
-        setImage (stoveCounter);
-        startingPot.scale(width - offset, width - offset);
-        startingPot();
-    }
-    /**
-     * Act - do whatever the stoveCounter wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        checkIfSelected(selectedStoveCounter, stoveCounter);
-        placeFood();
-    }
-    public void startingPot()
-    {
-        stoveCounter.drawImage(startingPot, 1, -8);
-    }
-    
-    public void placeFood()
-    {
-        if(checkIfSelected == true)
-        {
-            
-            World world = getWorld();
-            world.addObject(pots, getX(), getY());
-        }
-    }
 }
